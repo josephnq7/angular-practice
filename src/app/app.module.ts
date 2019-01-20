e16d89479a0ceac604b5619bc77b4fbcf36a621e
Joseph <quanghiepk7xl@gmail.com>
2019-01-20 15:09:49 -0500

    update source code

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpModule} from '@angular/http';
+import {FormsModule} from '@angular/forms'
+import {InMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {AppComponent} from './app.component';
+import {EntryListComponent, EntryComponent, EntryService, EntryCommentFormComponent} from './entries';
+import {InMemoryEntryService} from './backend'
+
+@NgModule({
+    imports: [
+        BrowserModule, 
+        HttpModule,
+        InMemoryWebApiModule.forRoot(InMemoryEntryService),
+        FormsModule
+    ],
+    providers: [ EntryService ],
+    declarations: [
+        AppComponent, 
+        EntryListComponent,
+        EntryComponent,
+        EntryCommentFormComponent
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule{
+
+}
---
+++ wwwroot/app.095698535282be2cdd74.js
@@ -0,0 +1,2 @@
+webpackJsonp([0],[function(t,e,c){"use strict";c(1)},function(t,e){t.exports="// removed by extract-text-webpack-plugin"}]);
+//# sourceMappingURL=app.095698535282be2cdd74.js.map
